4fc841dda723b96fe0f7c83dbfcae688c887f87d
AdrianPerera <nikiperera20@gmail.com>
2020-09-01 13:58:56 +0530

    input fields added and post request button

--- src/tabs/doctor/PatientViewTab.js
+++ src/tabs/doctor/PatientViewTab.js
@@ -12,12 +12,32 @@ import {
   Text,
   Button,
   Badge,
+  Input,
 } from 'native-base';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function PatientViewTab({route, navigation}) {
   const {name} = route.params;
-  
+  const state= {
+    hr:0,
+    min:0
+  }
+
+  const postRequest = () =>{
+    fetch('http://prevelcer.herokuapp.com/', {
+    method: 'POST',
+    headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    firstParam: 'yourValue',
+  })
+});
+
+  }
+
   return (
     <Container>
       <Header noLeft>
@@ -30,9 +50,36 @@ export default function PatientViewTab({route, navigation}) {
         </Right>
       </Header>
 
-
       <Content>
-        <Text> {name}</Text>
+        <Card>
+          <CardItem>
+            <Text> {name}</Text>
+          </CardItem>
+        </Card>
+
+        <Card>
+          <CardItem>
+            <Text> Next Reposition in </Text>
+          
+            <Input
+            keyboardType="number-pad"
+            placeholder="hr"
+            onChangeText={(text) => this.setState({hr: text})}
+          />
+
+          <Input
+          keyboardType="number-pad"
+          placeholder="min"
+          onChangeText={(text) => this.setState({min: text})}
+          />
+          </CardItem>
+        </Card>
+
+     
+          <Button onPress={()=>postRequest() }><Text> Post Request</Text></Button>
+          
+          
+      
       </Content>
     </Container>
   );
